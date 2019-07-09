41876ee7758f988fcffa476880ce873ccfd070e0
ahmed <ahmedfawzy671998@gmailcom>
2019-07-09 18:54:50 +0200

    add categories model and tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -62,6 +62,7 @@ class UsersController < ApplicationController
   def set_user
     @user = User.find(params[:id])
   end
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
