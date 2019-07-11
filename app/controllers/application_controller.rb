1b30b1fcd3c7aa5773152170a892b82e96f51409
ahmed <ahmedfawzy671998@gmailcom>
2019-07-11 05:31:13 +0200

    admin user cannot delete itself

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user, :logged_in?
-  def current_user; end
-  # @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 
   def logged_in?
     !!current_user
